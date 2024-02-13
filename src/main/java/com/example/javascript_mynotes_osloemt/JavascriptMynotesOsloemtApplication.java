544aed47c1e05a68403b85290a82fb555270786e
Huseyin Davutoglu <saskin@protonmail.com>
2024-02-13 13:40:21 +0100

    Initial commit

---
+++ src/main/java/com/example/javascript_mynotes_osloemt/JavascriptMynotesOsloemtApplication.java
@@ -0,0 +1,13 @@
+package com.example.javascript_mynotes_osloemt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavascriptMynotesOsloemtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavascriptMynotesOsloemtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javascript_mynotes_osloemt/JavascriptMynotesOsloemtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javascript_mynotes_osloemt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavascriptMynotesOsloemtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
